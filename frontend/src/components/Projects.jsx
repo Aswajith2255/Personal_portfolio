0ec48f696082007d6efcafbb776d4e17c3876a41
Aswajith2255 <aswajithpp63@gmail.com>
2026-04-19 23:04:12 +0530

    Update Projects.jsx

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -4,7 +4,7 @@ import img1 from '../assets/img1.png';
 import img2 from '../assets/img2.png';
 import img3 from '../assets/img3.png';
 import img4 from '../assets/img4.png';
-
+import img5 from '../assets/img5.png';
 export default function Projects() {
   const projectsData = [
     {
@@ -50,7 +50,7 @@ export default function Projects() {
       tags: ['React', 'Javascript', 'Html', 'Css',],
       image: img5,
       demoUrl: 'https://weatherapp-seven-rho.vercel.app/',
-      githubUrl: 'https://github.com/Aswajith2255/Weatherapp'
+      githubUrl: 'https://github.com/Aswajith2255/Weatherapp',
     }
     
   ];
